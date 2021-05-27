e804629b11b6ea10e9f196928952a651a10ecedc
qishuny <yuqishun1998@outlook.com>
2021-05-27 12:11:01 -0400

    read and plot force vs depth

--- platetest/readBag.m
+++ platetest/readBag.m
@@ -1,17 +1,72 @@
-[time1Data, forceData, time2Data, positionData]= read(2);
+%%
+robot = importrobot('urdf/irb120_3_58.urdf');
 
+[timeP1, forceData1, timeF1, positionData1]= read(1,robot);
+[timeP2, forceData2, timeF2, positionData2]= read(2,robot);
+[timeP3, forceData3, timeF3, positionData3]= read(3,robot);
+[timeP4, forceData4, timeF4, positionData4]= read(4,robot);
+[timeP5, forceData5, timeF5, positionData5]= read(5,robot);
+%% Trial 1
+zeroF1 = forceData1(1,3);
+force1 = forceData1(:,3)-zeroF1;
+force1 = -force1(1160:2321);
+zeroP1 = positionData1(1160,3);
+depth1 = positionData1(1160:2321,3)-zeroP1;
+depth1 = -depth1;
+
+
+%% Trial 2
+zeroF2 = forceData2(1,3);
+force2 = forceData2(:,3)-zeroF2;
+force2 = -force2(2500:3800);
+zeroP2 = positionData2(2500,3);
+depth2 = positionData2(2500:3800,3)-zeroP2;
+depth2 = -depth2;
+
+%% Trial 3
+zeroF3 = forceData3(1,3);
+force3 = forceData3(:,3)-zeroF3;
+force3 = -force3(2101:3301);
+zeroP3 = positionData3(2101,3);
+depth3 = positionData3(2101:3301,3)-zeroP3;
+depth3 = -depth3;
+
+%% Trial 4
+zeroF4 = forceData4(1,3);
+force4 = forceData4(:,3)-zeroF4;
+force4 = -force4(2453:3730);
+zeroP4 = positionData4(2453,3);
+depth4 = positionData4(2453:3730,3)-zeroP4;
+depth4 = -depth4;
+
+%% Trial 5
+zeroF5 = forceData5(1,3);
+force5 = forceData5(:,3)-zeroF3;
+force5 = -force5(2101:3361);
+zeroP5 = positionData5(2101,3);
+depth5 = positionData5(2101:3361,3)-zeroP5;
+depth5 = -depth5;
 
 figure
-plot(time1Data,positionData(:,3))
+
+plot(depth1,force1)
+hold on
+plot(depth2,force2)
+plot(depth3,force3)
+plot(depth4,force4)
+plot(depth5,force5)
+legend('trial 1','trial 2','trial 3','trial 4','trial 5');
+
+figure
+plot(timeP2,positionData2(:,3))
 legend('end effector z')
 
-
 figure
-plot(time2Data,forceData)
+plot(timeF2,forceData2)
 legend('force x', 'force y', 'force z')
 
 
-function [time1Data, forceData, time2Data, positionData]= read(trial)
+function [time1Data, forceData, time2Data, positionData]= read(trial,robot)
 
 switch trial
     case 1
@@ -34,39 +89,37 @@ end
 startT = bag.StartTime;
 endT = bag.EndTime;
 
-bagselect2 = select(bag, 'Time', [startT endT], 'Topic', '/data');
-msgs2 = readMessages(bagselect2);
-temp2 = msgs2{2};
+%% Force sensor data
 
-ts2 = timeseries(bagselect2, 'Wrench.Force.X', 'Wrench.Force.Y','Wrench.Force.Z');
+bagselectData = select(bag, 'Time', [startT endT], 'Topic', '/data');
+
+ts2 = timeseries(bagselectData, 'Wrench.Force.X', 'Wrench.Force.Y','Wrench.Force.Z');
 forceData = ts2.Data;
 time2Data = ts2.Time-startT;
 
-%%
-robot = importrobot('urdf/irb120_3_58.urdf');
+%% Joint states data
 
-bagselect1 = select(bag, 'Time', [startT endT],'Topic', '/joint_states');
-msgs1 = readMessages(bagselect1);
-numMess = bagselect1.NumMessages;
+bagselectJointStates = select(bag, 'Time', [startT endT],'Topic', '/joint_states');
+msgs1 = readMessages(bagselectJointStates);
+numMess = bagselectJointStates.NumMessages;
 
 positionData = zeros(numMess,3);
 config = homeConfiguration(robot);
 
 for i = 1:numMess
-    temp1 = msgs1{i};
-    
-    config(1).JointPosition = temp1.Position(1);
-    config(2).JointPosition = temp1.Position(2);
-    config(3).JointPosition = temp1.Position(3);
-    config(4).JointPosition = temp1.Position(4);
-    config(5).JointPosition = temp1.Position(5);
-    config(6).JointPosition = temp1.Position(6);
+    msgstemp = msgs1{i};  
+    config(1).JointPosition = msgstemp.Position(1);
+    config(2).JointPosition = msgstemp.Position(2);
+    config(3).JointPosition = msgstemp.Position(3);
+    config(4).JointPosition = msgstemp.Position(4);
+    config(5).JointPosition = msgstemp.Position(5);
+    config(6).JointPosition = msgstemp.Position(6);
     T = getTransform(robot, config, 'tool0');
     positionData(i,:) = [T(1,end), T(2,end), T(3,end)];
 end
 
 
-ts1 = timeseries(bagselect1);
+ts1 = timeseries(bagselectJointStates);
 time1Data = ts1.Time-startT;
 
 
